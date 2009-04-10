80759da65f238cbe08d8487a5a6ebda4861dc6df
Sergey V Turchin <zerg@altlinux.org>
2009-04-10 19:24:45 +0400

    - fix help browser behavior

--- apt-indicator/indicator/help_browser.cpp
+++ apt-indicator/indicator/help_browser.cpp
@@ -1,5 +1,6 @@
 
 #include <QProcess>
+#include <QDesktopServices>
 
 #include <help_browser.h>
 
@@ -7,40 +8,32 @@ HelpBrowser::HelpBrowser(QWidget *parent):
     QDialog(parent)
 {
     ui.setupUi(this);
+    ui.helpText->setOpenLinks(true);
+    ui.helpText->setOpenExternalLinks(false);
     connect( ui.helpText, SIGNAL( anchorClicked(const QUrl&) ), this, SLOT( execLink( const QUrl& ) ) );
 }
 
 void HelpBrowser::execLink( const QUrl &url )
 {
-    QString prog_command;
-    if( url.scheme() == "http" || url.scheme() == "https" )
+    if( url.scheme() == "file" )
     {
-	prog_command = getenv("HELP_BROWSER");
-	if( prog_command.isEmpty() )
+	ui.helpText->setSource(url);
+    }
+    else
+    {
+	if( !QDesktopServices::openUrl(url) )
 	{
-	    qWarning("%s", qPrintable(tr("HELP_BROWSER environment variable is empty")));
-	    prog_command = getenv("BROWSER");
-	    if( prog_command.isEmpty() )
-		qWarning("%s", qPrintable(tr("BROWSER environment variable is empty")));
+	    QString prog_command;
+	    prog_command = "url_handler.sh";
+
+	    qDebug("Try to open \"%s\" via %s", qPrintable(url.toString()), qPrintable(prog_command));
+
+	    QProcess *prog_proc = new QProcess(this);
+	    QStringList prog_args = prog_command.split(" ", QString::SkipEmptyParts);
+	    prog_args << url.toString() ;
+	    QString prog_name = prog_args.takeFirst();
+	    prog_proc->start(prog_name, prog_args);
 	}
+	ui.helpText->reload();
     }
-    else if( url.scheme() == "mailto" )
-    {
-	prog_command = getenv("MAILER");
-	if( prog_command.isEmpty() )
-	    qWarning("%s", qPrintable(tr("MAILER environment variable is empty")));
-    }
-    ui.helpText->reload();
-    
-    if( prog_command.isEmpty() )
-    {
-	prog_command = "url_handler.sh";
-	qDebug("Set prog_command to %s", qPrintable(prog_command));
-    }
-    
-    QProcess *prog_proc = new QProcess(this);
-    QStringList prog_args = prog_command.split(" ", QString::SkipEmptyParts);
-    prog_args << url.toString() ;
-    QString prog_name = prog_args.takeFirst();
-    prog_proc->start(prog_name, prog_args);
 }
