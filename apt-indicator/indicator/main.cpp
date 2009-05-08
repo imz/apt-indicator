9c275ee7a6051971fba26e0b691fba6ed42882dc
Sergey V Turchin <zerg@altlinux.org>
2009-05-08 13:35:24 +0000

    - cleanup

--- apt-indicator/indicator/main.cpp
+++ apt-indicator/indicator/main.cpp
@@ -5,8 +5,6 @@ Sergey V Turchin <zerg@altlinux.org>
 License: GPL
 */
 
-#include <getopt.h>
-
 #include <QApplication>
 #include <QTranslator>
 #include <QLocale>
@@ -20,57 +18,8 @@ License: GPL
 
 extern const char *__progname;
 
-void usage(int retcode)
-{
-    QTextStream out(stdout);
-    out << "Usage: " <<__progname<<" [-hva] [--help] [--version] [--autostart]";
-    out << "This is a simple applet both for Gnome and KDE which";
-    out <<  " made notifications for users that newer packages are available";
-    out << "  -h, --help             display help screen";
-    out << "  -v, --version          display version information";
-    out << "  -a, --autostart	       for running by Window Manager's autostart";
-    out << "";
-    out << "Report bugs to http://bugzilla.altlinux.ru";
-    exit(retcode);
-}
-
-
 int main( int argc, char **argv )
 {
-
-	bool autostart = false; //run under autostart
-
-	//process args
-	while (1)
-	{
-		static struct option long_options[] =
-			{
-				{"help", no_argument, 0, 'h'},
-				{"version", no_argument, 0, 'v'},
-				{"autostart", no_argument, 0, 'a'},
-				{0, 0, 0, 0}
-			};
-		int c = getopt_long(argc, argv, "hva", long_options, NULL);
-		if (c == -1)
-			break;
-		switch (c)
-		{
-		case 'h':
-			usage(EXIT_SUCCESS);
-			break;
-		case 'v':
-			qDebug() << __progname << " version " << VERSION;
-			qDebug() << "Written by Stanislav Ievlev and Sergey V Turchin";
-			qDebug() << "";
-			qDebug() << "Copyright (C) 2003-2004 ALT Linux Team";
-			return (EXIT_SUCCESS);
-		case 'a':
-			autostart = true;
-			break;
-		default:
-			usage(EXIT_FAILURE);
-		}
-	}
 
 	Q_INIT_RESOURCE(pixmaps);
 	QApplication app( argc, argv );
