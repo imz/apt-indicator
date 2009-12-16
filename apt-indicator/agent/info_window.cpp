6a5d461b2487b6e2e51460fd1330b71b3fe803b3
Sergey V Turchin <zerg@altlinux.org>
2009-12-16 14:11:38 +0300

    - allow to start synaptic without options

--- apt-indicator/agent/info_window.cpp
+++ apt-indicator/agent/info_window.cpp
@@ -10,10 +10,10 @@ InfoWindow::InfoWindow(QWidget *parent):
     setAttribute(Qt::WA_DeleteOnClose);
 
     QPushButton *btn_auto = buttonBox->addButton(tr("Automatic upgrade"), QDialogButtonBox::YesRole);
-//    QPushButton *btn_noauto = buttonBox->addButton(tr("Start upgrade program"), QDialogButtonBox::YesRole);
+    QPushButton *btn_noauto = buttonBox->addButton(tr("Run upgrade program"), QDialogButtonBox::YesRole);
 
     connect(btn_auto, SIGNAL(clicked()), this, SIGNAL(upgradeAuto()));
-//    connect(btn_noauto, SIGNAL(clicked()), this, SIGNAL(upgradeNoauto()));
+    connect(btn_noauto, SIGNAL(clicked()), this, SIGNAL(upgradeNoauto()));
 }
 
 InfoWindow::~InfoWindow()
